cb8b24bd5f0010c7cd49514668f99cf72ffcb110
Krzyminski <mikolaj.krzyminski@capgemini.com>
2022-02-19 15:11:52 +0100

    add routes file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello world! :D')
+import express from "express";
+// create expresss aplication
+const app = express();
+
+import router from "./routes";
+
+app.use("/", router);
+
+const port = 8000;
+
+app.listen(port, () => console.log(`App listen on PORT ${port}`));
